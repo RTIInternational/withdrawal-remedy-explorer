68e3971f820dd3c02834089cc776f3b986be8851
Sandy Preiss <apreiss@rti.org>
2021-08-18 09:33:22 -0400

    use edges and nodes as input data instead of edges_with_blocks.xlsx'

--- prepare_data.py
+++ prepare_data.py
@@ -1,17 +1,81 @@
 import pandas as pd
 import numpy as np
-from itertools import compress
+
+
+def ppmi(nodes: pd.DataFrame, edges: pd.DataFrame) -> pd.Series:
+    """
+    Calculates edge weights in terms of Positive Pointwise Mutual Information (PPMI).
+
+    PPMI scales the probability of two nodes co-occurring to the individual probability of
+    each node occurring. This decouples edge weights from node counts. Node pairs have high
+    PPMI when the probability of co-occurrence is only slightly lower than the probabilities
+    of occurrence of each node.
+    """
+    sum_node_counts = nodes["count"].sum()
+    sum_edge_counts = edges["edge_count"].sum()
+    edge_weights_ppmi = pd.Series(
+        np.log(
+            (edges["edge_count"] / sum_edge_counts)
+            / (
+                (edges["count_source"] / sum_node_counts)
+                * (edges["count_target"] / sum_node_counts)
+            )
+        ),
+    )
+    edge_weights_ppmi = np.where(edge_weights_ppmi < 0, 0, edge_weights_ppmi)
+    return edge_weights_ppmi
 
 
 def load_data():
-    edges = pd.read_excel(
-        "data/edges_with_blocks.xlsx", sheet_name="edges", engine="openpyxl"
-    )
+    edges = pd.read_parquet("data/edges.parquet")
+    nodes = pd.read_parquet("data/nodes.parquet")
 
-    remedy_edges = edges.query(
-        f"label_source == 'SUBSTANCE' \
-        and label_target == 'EFFECT'"
+    # This section transforms the input data into the format of the old
+    # edges_with_blocks.xlsx, which we used to use as input data for the app. Now we've
+    # transitioned to using the vanilla edge and node data, so some additional data
+    # processing is necessary.
+
+    edges["edge_count"] = edges["count"]
+    edges.drop(columns=["count"], inplace=True)
+    edges = edges.merge(nodes, how="inner", left_on="source", right_on="index")
+    edges = edges.merge(
+        nodes,
+        how="inner",
+        left_on="target",
+        right_on="index",
+        suffixes=("_source", "_target"),
     )
+    edges.drop(columns=["index_source", "index_target"], inplace=True)
+    edges.rename(
+        columns={"node_source": "source_text", "node_target": "target_text"},
+        inplace=True,
+    )
+    edges = edges.query("source_text != 'outlier' and target_text != 'outlier'")
+    edges_reversed = edges.copy()
+    edges_reversed.rename(
+        columns={
+            "source_text": "target_text",
+            "label_source": "label_target",
+            "count_source": "count_target",
+            "category_source": "category_target",
+            "target_text": "source_text",
+            "label_target": "label_source",
+            "count_target": "count_source",
+            "category_target": "category_source",
+        },
+        inplace=True,
+    )
+    edges = edges.append(edges_reversed)
+    remedy_edges = edges.query(
+        "label_source == 'SUBSTANCE' and label_target == 'EFFECT'"
+    ).copy()
+    remedy_nodes = nodes.query(
+        "node in @remedy_edges.source_text or node in @remedy_edges.target_text"
+    )
+    remedy_edges["ppmi"] = ppmi(remedy_nodes, remedy_edges)
+
+    # The rest of this function is the same data processing we used with
+    # edges_with_blocks.xlsx
 
     source_nodes = remedy_edges[
         [
